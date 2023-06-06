154f26440a8d4c9f7d8a6f411374b4376d2dcdba
blake S <design.harls@icloud.com>
2023-06-06 21:14:17 +0200

    for mini-ejercicios

---
+++ for.py
@@ -0,0 +1,17 @@
+# con indice
+for numbers in range(5):
+    print(numbers)
+
+# con indice y final
+for numbers_two in range(0, 11):
+    print(numbers_two)
+
+# con una busqueda de un numero
+found = 11
+for numero in range(21):
+    print(numero)
+    if numero == found:
+        print("Encontrado")
+        break
+    else:
+        print("No fue encontrado el numero")
