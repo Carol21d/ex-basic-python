5104093dd63274ca06903a02f47dee6d2fe6ba6e
blake S <design.harls@icloud.com>
2023-06-06 21:26:36 +0200

    while

---
+++ while.py
@@ -0,0 +1,10 @@
+number = 1
+
+while number < 100:
+    print(number)
+    number *= 2
+    if number < 100:
+        print("El numero es menor a 100")
+    else:
+        print(number)
+        print("El numero es mayor a 100")
