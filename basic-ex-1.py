e0501adee20881b32adc040c0dfef3bc00a786af
blake S <design.harls@icloud.com>
2023-06-07 20:05:42 +0200

    ejercicios de basicos de repaso

---
+++ basic-ex-1.py
@@ -0,0 +1,20 @@
+# ejercicios basicos de python org
+"""Try the exercises below
+-Make a program that displays several numbers.
+-Make a program that solves and shows the summation of 64 + 32.
+-Do the same as in 2, but make it sum x + y.
+"""
+
+# 1
+number_1 = 11
+number_2 = 22
+print(number_1, number_2)
+
+# 2
+
+numb_1 = 64
+numb_2 = 32
+numb_1 = int(numb_1)
+numb_2 = int(numb_2)
+result = numb_1 + numb_2
+print(f"el resultado es: {result}")
