078cea3dc9d603c95c8673384ab5d4c12104c668
blake S <design.harls@icloud.com>
2023-06-13 20:58:30 +0200

    hangman finished

--- hangman.py
+++ hangman.py
@@ -15,9 +15,22 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
-    user_letter = input("Guess a letter: ").upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
+
+    while len(word_letters) > 0:
+        print('You have used these letters:', ' '.join(used_letters))
+
+        word_list = [
+            letter if letter in used_letters else '-' for letter in word]
+        print('current word :', ''.join(word_list))
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        elif user_letter in used_letters:
+            print("You have already used that character. Try again")
+        else:
+            print("Invalid character, Try again")
 
 
 user_input = input('Type something: ')
