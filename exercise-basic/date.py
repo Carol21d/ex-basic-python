77a9ea32ec008622bb151ee912a48c6b5660ceb5
blake S <design.harls@icloud.com>
2023-06-15 19:25:06 +0200

    basic with date

--- exercise-basic/date.py
+++ exercise-basic/date.py
@@ -1,5 +1,7 @@
 ### Dates ###
 
+from datetime import date
+from datetime import time
 from datetime import datetime
 
 
@@ -9,18 +11,30 @@ now = datetime.now()
 def print_date(date):
     print(date.year)
     print(date.month)
+
     print(date.day)
     print(date.hour)
     print(date.minute)
     print(date.second)
+    print(date.timestamp())
 
 
 print_date(now)
 
 
-timestamp = now.timestamp()
-print(timestamp)
-
-
 year_2023 = datetime(2023, 1, 1)
 print_date(year_2023)
+
+
+current_time = time(21, 6, 0)  # aqui pasamos los parametros
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+
+current_time = date.today()
+print(current_time.year)
+print(current_time.month)
+print(current_time.day)
+
+current_time = date(2023, 6, 15)
