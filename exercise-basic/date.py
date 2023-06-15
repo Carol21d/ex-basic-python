0fa58b71f16b80d49d27045245053cb6f0d0f467
blake S <design.harls@icloud.com>
2023-06-15 19:11:24 +0200

    funcion  print_date

--- exercise-basic/date.py
+++ exercise-basic/date.py
@@ -5,13 +5,22 @@ from datetime import datetime
 
 now = datetime.now()
 
-print(now.year)
-print(now.month)
-print(now.day)
-print(now.hour)
-print(now.minute)
-print(now.second)
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+
+
+print_date(now)
+
 
 timestamp = now.timestamp()
-
 print(timestamp)
+
+
+year_2023 = datetime(2023, 1, 1)
+print_date(year_2023)
